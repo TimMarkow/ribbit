e767b747997840b543cd26727b63a67b9c91d231
Tim Markowski <tmarkowski@quantcast.com>
2013-03-07 13:50:20 -0800

    Created app and configured settings

--- ribbit/settings.py
+++ ribbit/settings.py
@@ -129,7 +129,7 @@ INSTALLED_APPS = (
     'south',
     'ribbit_app',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
